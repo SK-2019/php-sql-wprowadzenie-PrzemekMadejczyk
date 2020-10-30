4dadcb8110ee1257fb77abaad3cb40ab278865a3
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-10-30 13:18:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,5 +20,28 @@ if ($result->num_rows > 0) {
 } else {
   echo "0 results";
 }
+
+
+$ sql = "select * from pracownicy where imie not like '%a'" ;
+$ wynik = $ conn -> zapytanie ( $ sql );
+        echo ( „<h3> Tabela Pracowników </h3>” );
+        echo ( "<obramowanie tabeli = 1>" );
+        echo ( "<th> id </th>" );
+        echo ( "<th> imie </th>" );
+        echo ( "<th> dzial </th>" );
+        echo ( "<th> zarobki </th>" );
+
+if ( $ wynik -> num_rows > 0 ) {
+  // dane wyjściowe każdego wiersza
+  while ( $ row = $ wynik -> fetch_assoc ()) {
+    echo ( "<tr>" );
+    echo ( "<td>" . $ row [ 'id_pracownicy' ]. "</td> <td>" . $ row [ 'imie' ]. "</td> <td>" . $ row [ 'dzial' ]. "</td> <td>" . $ row [ 'zarobki' ]. "</td>" );
+    echo ( „</tr>” );
+  }
+  echo ( „</table>” );
+} else {
+  echo  "0 wyników" ;
+}
+
 $conn->close();
 ?>
