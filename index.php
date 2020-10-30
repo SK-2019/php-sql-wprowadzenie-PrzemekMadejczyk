ca4ffe74f0dbc0fd4808ef05cc58b0ef6a9249e3
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-10-30 12:57:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 <?php
-$servername = "mysql-przemek-madejczyk.alwaysdata.net";
-$username = "217186";
-$password = "przemekenter1";
-$dbname = "przemek-madejczyk_123";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
 $sql = "SELECT * from pracownicy";
 $result = $conn->query($sql);
         echo("<h3>Tabela Pracowników</h3>");
