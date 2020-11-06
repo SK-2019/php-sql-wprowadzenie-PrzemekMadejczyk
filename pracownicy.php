0b54d8887356da4d8bcd77b994787ab06a2a1d51
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-06 09:01:20 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -5,21 +5,24 @@
 </head>
 <body>
 <?php
-    require_once('conn.php');;
-    $result=$conn->query('SELECT * FROM pracownicy,organizacja where dzial=id_org');//mysql
-        echo("<h3>Tabela Pracowników</h3>");
+   require_once('log.php');
+$sql = "SELECT * from pracownicy";
+$result = $conn->query($sql);
+        echo("<h3>zadanie 1</h3>");
+       echo("<h3>$sql</h3>");
         echo("<table border=1>");
         echo("<th>id</th>");
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-        echo("<th>data_urodzenia</th>");
-            while($row=$result->fetch_assoc()){
-                echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
-                echo("</tr>");
-            }
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+    echo("</tr>");
+  }
         echo("</table>");
         echo("<hr />");
         ?>
