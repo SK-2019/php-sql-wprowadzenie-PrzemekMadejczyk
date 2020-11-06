e3ef65a74213d9fa025a7306f92870584573b5e7
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-06 12:44:20 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -7,6 +7,7 @@
     <div class="nav">
     <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
     <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
+       <a class="nav_link" href="index.php">Strona Główna</a>
 </div>>
 <?php
     require_once('log.php');;
