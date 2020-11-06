174eb6c78931b3177f2637e9d878021ed117f36b
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-06 12:11:54 +0100

    Update funkcjeagregujace.php

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -29,9 +29,9 @@
 echo("<hr />");
     
         $sql = "SELECT dzial,sum(zarobki) as suma from pracownicy group by dzial";
-    $result=$conn->query($sql);
-        echo("<h3>zadanie 1</h3>");
-      echo("<table border=1>");                
+    $result=$conn->query($sql)
+    echo("<h3>zadanie 2</h3>");
+    echo("<table border=1>");                
     echo("<th>dzial</th>");
     echo("<th>suma</th>");
     while($row=$result->fetch_assoc()){
@@ -40,7 +40,36 @@ echo("<hr />");
     echo("</tr>");
 
   }
+    echo("</table>");
+    echo("<hr />");
+    
+
+    $sql = "SELECT dzial,min(zarobki) as minimalna from pracownicy group by dzial";
+    $result=$conn->query($sql);
+    echo("<h3>zadanie 3</h3>");
+    echo("<table border=1>");                
+    echo("<th>dzial</th>");
+    echo("<th>minimalna</th>");
+        while($row=$result->fetch_assoc()){
+    echo("<tr>");
+    echo("<td>".$row["dzial"]."</td><td>".$row["minimalna"]."</td>"); 
+    echo("</tr>");
+
+  }
   echo("</table>");
 echo("<hr />");
-    
+        $sql = "SELECT dzial,max(zarobki) as maksymalna from pracownicy group by dzial";
+    $result=$conn->query($sql);
+    echo("<h3>zadanie 3</h3>");
+    echo("<table border=1>");                
+    echo("<th>dzial</th>");
+    echo("<th>maksymalna</th>");
+        while($row=$result->fetch_assoc()){
+    echo("<tr>");
+    echo("<td>".$row["dzial"]."</td><td>".$row["maksymalna"]."</td>"); 
+    echo("</tr>");
+
+  }
+  echo("</table>");
+echo("<hr />");
 ?>
