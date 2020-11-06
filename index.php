b0860ddc33ee53654116b3eafea3550f29e5bdab
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-06 08:41:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<strong><a href="https://https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk>Github</a></strong>
+<strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a></strong>
 <h1>Przemek Madejczyk 2ti 20</h1>
 <div class="nav">
         <a class ="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
