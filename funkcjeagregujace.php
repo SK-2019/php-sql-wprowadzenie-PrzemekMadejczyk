7c1cad26f5eaffe28bd2a34cb8f160ade8599972
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-06 12:19:54 +0100

    Update funkcjeagregujace.php

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -29,13 +29,24 @@
 echo("<hr />");
     $sql = "SELECT dzial,sum(zarobki) as suma from pracownicy group by dzial";
     $result=$conn->query($sql);
-        echo("<h3>zadanie 1</h3>");
+        echo("<h3>zadanie 2</h3>");
       echo("<table border=1>");                
     echo("<th>dzial</th>");
     echo("<th>suma</th>");
     while($row=$result->fetch_assoc()){
     echo("<tr>");
     echo("<td>".$row["dzial"]."</td><td>".$row["suma"]."</td>"); 
+    echo("</tr>");
+        
+            $sql = "SELECT dzial,min(zarobki) as minimalna from pracownicy group by dzial";
+    $result=$conn->query($sql);
+        echo("<h3>zadanie 3</h3>");
+      echo("<table border=1>");                
+    echo("<th>dzial</th>");
+    echo("<th>minimalna</th>");
+    while($row=$result->fetch_assoc()){
+    echo("<tr>");
+    echo("<td>".$row["dzial"]."</td><td>".$row["minimalna"]."</td>"); 
     echo("</tr>");
 
   }
