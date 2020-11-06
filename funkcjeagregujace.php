0a0ac3dace8e8529d2c4d046b64b016c3a346df2
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-06 10:02:52 +0100

    Update funkcjeagregujace.php

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -16,16 +16,15 @@
     $sql = "SELECT dzial,avg(zarobki) as srednia from pracownicy group by dzial";
 $result = $conn->query($sql);
         echo("<h3>zadanie 3</h3>");
-      echo("<h3>$sql</h3>");
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-  while($row = $result->fetch_assoc()) {
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
-    echo("</tr>");
+      echo("<table border=1>");                
+                    echo("<th>dział</th>");
+                    echo("<th>średnia</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                                echo("<td>".$row["dzial"]."</td><td>".$row["srednia"]."</td>");
+                            echo("</tr>");
+                            }
+                    echo("</table>");
   }
   echo("</table>");
 echo("<hr />");
