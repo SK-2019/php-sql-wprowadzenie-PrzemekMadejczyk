521f2b3caf95b08efec69d18f4f2d0495aa556bc
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-06 09:49:21 +0100

    Update funkcjeagregujace.php

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -1,47 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="style1.css">
-<a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk"> GitHub </a>
-<div class="nav">
-        <a class ="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
-        <a class ="nav_link" href="funkcAgregujace.php">Funkcje Agregujace</a>
-</div>  
-
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
 <h1>Przemek Madejczyk 2Ti</h1>  
 <h3>Funkcje Agregujące</h3>  
 <?php
-   $sql = "SELECT dzial,sum(zarobki) as suma from pracownicy group by dzial";
-$result = $conn->query($sql);
-        echo("<h3>zadanie 3</h3>");
-      echo("<h3>$sql</h3>");
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-  while($row = $result->fetch_assoc()) {
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
-    echo("</tr>");
-  }
-  echo("</table>");
-echo("<hr />");
-    $sql = SELECT dzial,avg(zarobki) as srednia from pracownicy group by dzial";
-$result = $conn->query($sql);
-        echo("<h3>zadanie 3</h3>");
-      echo("<h3>$sql</h3>");
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-  while($row = $result->fetch_assoc()) {
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
-    echo("</tr>");
-  }
-  echo("</table>");
-echo("<hr />");
+        require_once('log.php');;
+            $result=$conn->query('SELECT dzial,avg(zarobki) as srednia from pracownicy group by dzial');//mysql
+echo ("<h3>Funkcje Agregujące</h3");
+               $result=$conn->query($sql);
+                    echo("<table border=1>");
+                    echo("<h3>ZAD $nr_zad</h3>");
+                    echo("<li>SQL: $sql");
+                    echo("<th>dział</th>");
+                    echo("<th>średnia</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                                echo("<td>".$row["dzial"]."</td><td>".$row["srednia"]."</td>");
+                            echo("</tr>");
+                            }
+                    echo("</table>");
+                    echo("<hr />");
+                }
