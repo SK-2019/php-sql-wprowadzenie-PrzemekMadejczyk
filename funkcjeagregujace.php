0d0fb72d60e32edf1e4fdf0d517e0e2d280d375f
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-06 09:29:28 +0100

    Update funkcjeagregujace.php

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -1,1 +1,53 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style1.css">
+<a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk"> GitHub </a>
+<div class="nav">
+    <a class="nav_link" href="index.php">Strona Główna</a>    
+    <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
+</div>
+</head>
+<body>
+<h1>Przemek Madejczyk 2Ti</h1>  
+<h3>Funkcje Agregujące</h3>  
+<?php
+   echo("<hr />")
+      $sql=('SELECT dzial, sum(zarobki) as suma  FROM pracownicy,organizacja where dzial=id_org');
+    $result=$conn->query($sql);
+        echo("<hr />");
+         echo("<h3>zadanie 1 </h3>");;
+        echo("<li>SQL: $sql");
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imię</th>");
+        echo("<th>dział</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>nazwa działu</th>");
+        echo("<th>data urodzenia</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+    echo("<hr />");
+   $sql=('SELECT dzial, avg(zarobki) as srednia  FROM pracownicy,organizacja where dzial=id_org');
+    $result=$conn->query($sql);
+        echo("<hr />");
+         echo("<h3>zadanie 1 </h3>");;
+        echo("<li>SQL: $sql");
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imię</th>");
+        echo("<th>dział</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>nazwa działu</th>");
+        echo("<th>data urodzenia</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+    echo("<hr />");
