32c75f4056c567e3c54e25de9dd4be0982f69f6f
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-06 09:34:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -60,19 +60,13 @@ $result = $conn->query($sql);
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
-
-if ($result->num_rows > 0) {
-  // output data of each row
   while($row = $result->fetch_assoc()) {
     echo("<tr>");
     echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
     echo("</tr>");
   }
   echo("</table>");
-} else {
-  echo "0 results";
-}
-
+echo("<hr />");
 
 
 $conn->close();
