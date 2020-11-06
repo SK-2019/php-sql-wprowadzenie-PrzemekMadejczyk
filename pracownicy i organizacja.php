3a2cc8897db113f204691d52123a5b413d51295c
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-06 13:35:20 +0100

    Update pracownicy i organizacja.php

--- pracownicy i organizacja.php
+++ pracownicy i organizacja.php
@@ -52,4 +52,24 @@ require_once('log.php');
             }
         echo("</table>");
     echo("<hr />");;
+    
+    $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org and dzial=1 or dzial=4 and imie NOT LIKE '%a'');
+    $result=$conn->query($sql);
+        echo("<hr />");
+        echo("<h3>Tabela Pracowników</h3>");
+        echo("<li>SQL: $sql");
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imię</th>");
+        echo("<th>dział</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>nazwa działu</th>");
+        echo("<th>data urodzenia</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+    echo("<hr />");;
 
