d569ac18df25f02bc49524e18599f508fc0f4539
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-10 12:22:49 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -4,10 +4,12 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="nav">
-    
+<strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a></strong>
+<h1>Przemek Madejczyk 2ti 20</h1>
+<div class="nav">
+    <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
     <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
-       <a class="nav_link" href="index.php">Strona Główna</a>
+    <a class="nav_link" href="pracownicy i organizacja.php">Pracownicy i Organizacja</a>
 </div>>
     <h1>Pracownicy</h1>
 <?php
