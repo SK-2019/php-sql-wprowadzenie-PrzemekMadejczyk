ca5f1c3921da06c87b4e3c889025ae0de99ea3c5
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-13 09:02:25 +0100

    Update Data_czas.php

--- Data_czas.php
+++ Data_czas.php
@@ -39,3 +39,27 @@ require_once('log.php');
                             }
                         echo("</table>");
                         echo("<hr />");
+                                                                                    
+    
+    
+        echo("<h3>zadanie 2</h3>");
+        echo("<h3>Wiek poszczególnych pracowników (w latach) z działu serwis</h3>");
+                $sql=('SELECT * ,YEAR(curdate())-YEAR(data_urodzenia) as wiek FROM pracownicy,organizacja where dzial=id_org and nazwa_dzial="serwis"');
+                $result=$conn->query($sql);//mysql
+        echo("<table border=1>");
+        echo("<li>SQL: $sql");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>nazwa działu</th>");
+        echo("<th>wiek</th>");
+        echo("<th>data_urodzenia</th>");
+            while($row=$result->fetch_assoc())
+        {
+            echo("<tr>");
+                echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['wiek']."</td><td>".$row['data_urodzenia']."</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
+        echo("<hr />");
