1ba8630ffc3631b9401dc08cc63d897a5a1501e4
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-13 08:45:17 +0100

    Update Data_czas.php

--- Data_czas.php
+++ Data_czas.php
@@ -11,7 +11,7 @@
     <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
     <a class="nav_link" href="pracownicy i organizacja.php">Pracownicy i Organizacja</a>
      <a class="nav_link" href="Data_czas.php">Data i czas</a>
-  
+    <a class="nav_link" href="index.php">strona głowna</a>
   
   <a class="nav_link" href="index.php">strona głowna</a>
 </div>
