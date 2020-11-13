4f262eafde839d3f440bbac013ee1a5844ab5e12
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-13 08:27:17 +0100

    Update funkcjeagregujace.php

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -182,19 +182,24 @@ echo("<hr />");
         echo("</table>");
 echo("<hr />");
    
-      $sql=('SELECT nazwa_dzial,avg(zarobki) as srednia from pracownicy,organizacja where dzial=id_org group by dzial');
+             echo("<h2>Having</h2>");
+            echo("<hr />");
+
+            
+            $sql=('SELECT nazwa_dzial,sum(zarobki)as suma from pracownicy, organizacja where dzial=id_org GROUP BY nazwa_dzial HAVING sum(zarobki) < 28 ');
     $result=$conn->query($sql); //mysql
-        echo("<h3>Średnie zarobków w poszczególnych działach</h3>");//nazwa nad tabelą
+        echo("<h3>Suma zarobków w poszczególnych działach mniejsza od 28</h3>");//nazwa nad tabelą
         echo("<table border=1>");
         echo("<li>SQL: $sql");
-        echo("<th>srednia</th>");
+        echo("<th>suma</th>");
         echo("<th>nazwa działu</th>");
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['srednia']."</td><td>".$row['nazwa_dzial']."</td>");
+                    echo("<td>".$row['suma']."</td><td>".$row['nazwa_dzial']."</td>");
                 echo("</tr>");
             }
         echo("</table>");
-echo("<hr />");
+        echo("<hr>");
+
    
 ?>
