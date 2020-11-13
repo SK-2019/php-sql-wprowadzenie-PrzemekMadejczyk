1e91ac52f2424af047b5d09970ccf3205e353fb3
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-13 08:44:34 +0100

    Update Data_czas.php

--- Data_czas.php
+++ Data_czas.php
@@ -1,1 +1,19 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a></strong>
+<h1>Przemek Madejczyk 2ti 20</h1>
+<div class="nav">
+    <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
+    <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
+    <a class="nav_link" href="pracownicy i organizacja.php">Pracownicy i Organizacja</a>
+     <a class="nav_link" href="Data_czas.php">Data i czas</a>
+  
+  
+  <a class="nav_link" href="index.php">strona głowna</a>
+</div>
+<?php
+require_once('log.php');
