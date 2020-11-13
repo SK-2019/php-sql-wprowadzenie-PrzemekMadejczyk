e63b48415fbd99236369cb47c71369494e5e22c0
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-13 13:50:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,12 +19,13 @@ require_once('log.php');
                
     $result=$conn->query('SELECT * FROM pracownicy,organizacja where dzial=id_org and dzial=2');//mysql
         echo("<table border=1>");
+  echo("<li>SQL: $sql");
         echo("<th>id</th>");
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
         echo("<th>nazwa_dzial</th>");
-          echo("<li>SQL: $sql");
+          
         echo("<th>data_urodzenia</th>");
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
@@ -36,13 +37,14 @@ require_once('log.php');
 
         $result=$conn->query('SELECT * FROM pracownicy,organizacja where dzial=id_org and dzial=2 or dzial=3');//mysql
         echo("<table border=1>");
+  echo("<li>SQL: $sql");
         echo("<th>id</th>");
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
         echo("<th>nazwa_dzial</th>");
         echo("<th>data_urodzenia</th>");
-   echo("<li>SQL: $sql");
+   
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
                     echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
@@ -53,13 +55,14 @@ require_once('log.php');
         
         $result=$conn->query('SELECT * FROM pracownicy,organizacja where dzial=id_org and zarobki<30');//mysql
         echo("<table border=1>");
+    echo("<li>SQL: $sql");
         echo("<th>id</th>");
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
         echo("<th>nazwa_dzial</th>");
         echo("<th>data_urodzenia</th>");
-   echo("<li>SQL: $sql");
+ 
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
                     echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
