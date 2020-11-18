e01d474a24e4ec8b321a0413dafee3b43c7f71ff
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-18 14:40:02 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,9 +1,19 @@
+<!DOCTYPE html>
+<html>
 <?php
 
-echo("jesteś na stronie.php");<br>
+echo("jesteś na stronie.php");
+echo("<li>imię:".$_POST["imie"]);
+echo("<li>nazwisko:".$_POST["nazwisko"]);
 
-echo($_POST["imie"]);<br>
-
-echo($_POST["nazwisko"]);<br>
-
+   require_once('conn.php');
+  $sql = "INSERT INTO MyGuests (imie, nazwisko)
+  VALUES ('John', 'Doe')";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
 ?>
