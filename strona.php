e2de6bbd458988fca637e19c86dec50e095f8c39
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-18 15:06:20 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -7,7 +7,7 @@ echo("<li>imię:".$_POST["imie"]);
 echo("<li>nazwisko:".$_POST["nazwisko"]);
 
    require_once('log.php');
-$sql = "INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (null,Przemek,1,80,1975-01-28)"
+$sql = "INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (null,'Przemek',1,80,'1975-01-28')"
   
   if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
