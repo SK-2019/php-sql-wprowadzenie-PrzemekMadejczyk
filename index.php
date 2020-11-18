b5ae5c78bf4270540a5e03b2517ffc849a3d5d6f
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-18 15:05:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -40,24 +40,7 @@ require_once('log.php');
         echo("</table>");
     echo("<hr />");
   
-   $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org and dzial=2');
-        $result=$conn->query($sql); //mysql
-        echo("<table border=1>");
-  echo("<li>SQL: $sql");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>nazwa_dzial</th>");
-          
-        echo("<th>data_urodzenia</th>");
-            while($row=$result->fetch_assoc()){
-                echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-        echo("<hr />");
+ 
         
         $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org and dzial=2 or dzial=3');
         $result=$conn->query($sql);
