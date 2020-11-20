2dab36b30597defc2ee6370394e70801a926ba77
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-20 13:17:48 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,30 +2,20 @@
 <!DOCTYPE html>
 <html>
 <?php
-  
-echo("jestes w insert.php <br>");
+
+   
+echo("jesteś na stronie.php");
 echo("<li>imię:".$_POST["imie"]);
 echo("<li>dział:".$_POST["dzial"]);
 echo("<li>zarobki:".$_POST["zarobki"]);
 echo("<li>data urodzenia:".$_POST["data_"]);
-  
+
    require_once('conn.php');
-  //if ($conn->connect_error) {
-  //die("Connection failed: " . $conn->connect_error);
-//}
-   $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES
-   (NULL,'".$_POST['imie']."',
-   '".$_POST['dzial']."', 
-   '".$_POST['zarobki']."', 
-   '".$_POST['data_']."')";
+   $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_']."')";
    if ($conn->query($sql) === TRUE) {
-   echo("<br>"); 
-   echo "New record created successfully";
-    }
-  else 
-  {
+    echo("<br>"); 
+    echo "New record created successfully";
+  } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
   }
   $conn->close();
-?>
-
