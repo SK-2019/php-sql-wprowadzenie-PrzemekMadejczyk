5cf170df72171331301bddfe566f04d4d169a1fb
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-20 08:50:31 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -8,12 +8,13 @@ echo("<li>dział:".$_POST["dzial"]);
 echo("<li>zarobki:".$_POST["zarobki"]);
 echo("<li>data urodzenia:".$_POST["data_ur"])
    require_once('log.php');
-$sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_ur']."')";
-  
-  if ($conn->query($sql) === TRUE) {
+$sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_']."')";
+   if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
   } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: " . $sql . "<br>" . $log->error;
   }
-  $conn->close();;
+  $log->close();
+?>
+
 ?>
