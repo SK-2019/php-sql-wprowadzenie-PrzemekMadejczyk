eafe9af08aeb46511afc09f4e622c7701f9bc0a2
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-20 13:03:16 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,6 +4,4 @@ $sql =  "DELETE FROM pracownicy where id_pracownicy='".$_POST['id']."'";
 echo($sql);
 mysqli_query($conn,$sql);
 mysqli_close($conn);
-header("location:https://index-a.herokuapp.com/formularz");
-
 ?>
