8b81c8c1c95a69ca39282cddfb964ff4b26dad79
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-20 13:15:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
   <a class="nav_link" href="index.php">strona głowna</a>
   <a class="nav_link" href="Data_czas.php">Data i czas</a>
   <a class="nav_link" href="formularz.html">Formularze</a>
-  
+  <a class="nav_link" href="insert.php">insert</a>
 </div>
 <?php
 require_once('conn.php');
