909b4404f07a492187c5df986bcc750f7814a6fb
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-20 13:35:57 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,23 +2,38 @@
 
 <!DOCTYPE html>
 <html>
+<head>
+
+  </form
+</head>
+  <link rel="stylesheet" href="style.css">
+<body>
+<strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a></strong>
+<h1>Przemek Madejczyk 2ti 20</h1>
+<div class="nav">
+    <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
+    <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujace</a>
+    <a class="nav_link" href="pracownicy i organizacja.php">Pracownicy i Organizacja</a>
+  <a class="nav_link" href="index.php">strona glowna</a>
+  <a class="nav_link" href="Data_czas.php">Data i czas</a>
+  <a class="nav_link" href="formularz.html">Formularze</a>
+</div>
+</html> 
 <?php
 
-   
-echo("jesteś na stronie.php");
-echo("<li>imię:".$_POST["imie"]);
-echo("<li>dział:".$_POST["dzial"]);
-echo("<li>zarobki:".$_POST["zarobki"]);
-echo("<li>data urodzenia:".$_POST["data_"]);
+echo("<li>imię: ".$_POST['imie']."</li>");
+echo("<li>dział: ".$_POST['dzial']."</li>");
+echo("<li>zarobki: ".$_POST['zarobki']."</li>");
+echo("<li>data urodzenia: ".$_POST['data_ur']."</li>");
 
-   require_once('conn.php');
-   $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_']."')";
-   if ($conn->query($sql) === TRUE) {
-    echo("<br>"); 
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-  }
-  $conn->close();
-?>
+
+ require_once("connect.php");
+	$sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_ur']."')";
+	
+if ($conn->query($sql) === TRUE) {
+        echo("<p class='precord'>Dodano nowego pracownika  </p>");
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+
 
