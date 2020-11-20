20bf4dafd2560370c42e964424c4bcb903487abc
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-20 08:57:13 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -7,7 +7,7 @@ echo("<li>imię:".$_POST["imie"]);
 echo("<li>dział:".$_POST["dzial"]);
 echo("<li>zarobki:".$_POST["zarobki"]);
 echo("<li>data urodzenia:".$_POST["data_ur"])
-   require_once('log.php');
+   require_once('conn.php');
  $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_']."')";
    if ($conn->query($sql) === TRUE) {
     echo("<br>"); 
