ae1749a488f5c52f18c6d4c720939a5c5b7fd69c
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-20 13:32:31 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,29 +1,2 @@
 
-<!DOCTYPE html>
-<html>
-<head>
 
-  </form
-</head>
-  <link rel="stylesheet" href="style.css">
-<body>
-<strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a></strong>
-<h1>Przemek Madejczyk 2ti 20</h1>
-<div class="nav">
-    <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
-    <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujace</a>
-    <a class="nav_link" href="pracownicy i organizacja.php">Pracownicy i Organizacja</a>
-  <a class="nav_link" href="index.php">strona glowna</a>
-  <a class="nav_link" href="Data_czas.php">Data i czas</a>
-  <a class="nav_link" href="formularz.html">Formularze</a>
-</div>
-
-<form action="strona.php" method="POST">
-   <input type="text" name="imie" placeholder="Imie">
-   <br><input type="text" name="dzial" placeholder="Dzial">
-   <br><input type="text" name="zarobki" placeholder="Zarobki">
-   <br><input type="date" name="data_">
-   <input type="submit" value="Dodaj">
-   </form>
-
-</html>
