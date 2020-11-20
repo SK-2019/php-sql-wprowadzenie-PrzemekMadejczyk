3a6c6405bfb351d77980c913077dc0fde716f483
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-20 08:31:15 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -3,9 +3,20 @@
 <?php
 
 echo("jesteś na stronie.php");
-echo("<li>imię:".$_POST["imie"]);
-echo("<li>nazwisko:".$_POST["nazwisko"]);
-
+echo("<h3>Imie</h3>");
+echo($_POST["imie"]);
+echo("</br>");
+echo("<h3>Nazwisko</h3>");
+echo($_POST["nazwisko"]);
+echo("</br>");
+echo("<h3>Pesel</h3>");
+echo($_POST["pesel"]);
+echo("</br>");
+echo("<h3>Urodziny</h3>");
+echo($_POST["urodziny"]);
+echo("</br>");
+echo("<h3>Adres</h3>");
+echo($_POST["adres"]);
    require_once('log.php');
 $sql = "INSERT INTO `pracownicy`(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES (null,"Przemek",2,40,1996-01-11)";"
   
