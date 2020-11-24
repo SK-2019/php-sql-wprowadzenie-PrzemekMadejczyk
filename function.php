e5ecf4dfa9dc5907901712a4a7423603a8ebc55a
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-24 13:09:07 +0100

    Update function.php

--- function.php
+++ function.php
@@ -30,5 +30,23 @@ function funkcja()
     }
 }
 funkcja();
-
+function robot($sql){
+    require_once("conn.php");
+    $result=$conn->query($sql);
+        echo("<table border=1>");
+        echo("<li>SQL: $sql");
+        echo("<th>id</th>");
+        echo("<th>imię</th>");
+        echo("<th>dział</th>");
+        echo("<th>zarobki</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+        }
+robot('SELECT * FROM pracownicy');
+  
+  
 ?>
