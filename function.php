17da3627f259ff6d77f83ee7128304dd092be2f5
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-24 12:57:28 +0100

    Create function.php

---
+++ function.php
@@ -0,0 +1,13 @@
+<?php
+echo("asd");
+function nazwajakas()
+{
+  echo("<li>teraz działa funkcja");
+  echo("<li test ");
+  for($i =1; $i <10; $i++)
+  {
+  echo("lito jest w petli: ".$i);
+  }
+}
+nazwajakas();
+?>
