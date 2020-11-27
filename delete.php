b246e2603c95287e6d8cb351571b575f48cf35ab
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-27 08:38:14 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -21,33 +21,6 @@
 <?php
 
 
-     <?php
-echo ("<h1>id: ".$_POST['id']."</h1>");
-require_once("conn.php");
-
- $sql = "DELETE FROM pracownicy WHERE id_pracownicy='".$_POST['id']."'";
-
-if ($conn->query($sql) === TRUE) {
-        echo("<h1 class='precord'> Usunięto pracownika </h1>");
-      } else {
-        echo("<h1 class='precord'>'Error: ' . $sql . '<br>' . $conn->error</h1>");
-      }
-   
-	    echo("<h1>Tabela  Pracowników:</h1>");
-$result = $conn->query('SELECT * FROM pracownicy,organizacja WHERE dzial = id_org');       
-        echo("<table>");      
-        echo("<th>ID</th>");
-        echo("<th>Imie</th>");
-        echo("<th>Dział</th>");
-        echo("<th>Zarobki</th>");
-        echo("<th>Data_Urodzenia</th>");
-        echo("<th>nazwa_dzialu</th>");
-            while($row=$result->fetch_assoc()){ 
-                echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
-
-                echo("</tr>");
-            }
-        echo("</table>");
+    
       
 ?>
