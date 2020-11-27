4035dbd7b73c340d4ff26d59534f99b061104fa7
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-27 09:22:48 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,62 +2,24 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-
-  </form
-</head>
-  <link rel="stylesheet" href="style.css">
-<body>
-<strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a></strong>
-<h1>Przemek Madejczyk 2ti 20</h1>
-<div class="nav">
-        <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
-    <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
-    <a class="nav_link" href="pracownicy i organizacja.php">Pracownicy i Organizacja</a>
-  <a class="nav_link" href="index.php">strona głowna</a>
-  <a class="nav_link" href="Data_czas.php">Data i czas</a>
-  <a class="nav_link" href="formularz.html">Formularze</a>
-  <a class="nav_link" href="daneDoBazy.php">dane do bazy danych</a>
-  <a class="nav_link" href="function.php">funkcja</a>
-</div>
-</html> 
-<?php
- 
 <?php
 
    
-echo("<li>Imie: ".$_POST['imie']."</li>");
-echo("<li>Dzial: ".$_POST['dzial']."</li>");
-echo("<li>Zarobki: ".$_POST['zarobki']."</li>");
-echo("<li>Data urodzenia: ".$_POST['data_ur']."</li>");
+echo("jesteś na stronie.php");
+echo("<li>imię:".$_POST["imie"]);
+echo("<li>dział:".$_POST["dzial"]);
+echo("<li>zarobki:".$_POST["zarobki"]);
+echo("<li>data urodzenia:".$_POST["data_"]);
 
-  require_once("conn.php");
-
-   $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_ur']."')";
+   require_once('conn.php');
+   $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_']."')";
    if ($conn->query($sql) === TRUE) {
     echo("<br>"); 
     echo "New record created successfully";
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
   }
- 
- 
- $sql=('SELECT * FROM pracownicy');
-    $result=$conn->query($sql);
-        echo("<hr />");
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imię</th>");
-        echo("<th>dział</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>data urodzenia</th>");
-            while($row=$result->fetch_assoc()){
-                echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-$conn->close(); 
+  $conn->close();
 ?>
 
 
