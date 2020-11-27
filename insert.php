ebf88e29640cac03f7d0a9c90a9192f21157878f
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-27 09:13:51 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -23,27 +23,24 @@
 </html> 
 <?php
  
-echo("jestes w insert.php <br>");
-echo "<li>". $_POST['name'];
-echo "<li>". $_POST['dzial'];
-echo "<li>". $_POST['zarobki'];
-echo "<li>". $_POST['data_'];
+<?php
 
+   
+echo("jesteś na stronie.php");
+echo("<li>imię:".$_POST["imie"]);
+echo("<li>dział:".$_POST["dzial"]);
+echo("<li>zarobki:".$_POST["zarobki"]);
+echo("<li>data urodzenia:".$_POST["data_"]);
 
-require_once('conn.php');
-//definiujemy zapytanie $sql
-$sql = "sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`)
-  VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_']."')";
-
-//wyświetlamy zapytanie $sql
-echo "<li>". $sql;
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+   require_once('conn.php');
+   $sql = "INSERT INTO pracownicy(`id_pracownicy`, `imie`, `dzial`, `zarobki`, `data_urodzenia`) VALUES(NULL,'".$_POST['imie']."', '".$_POST['dzial']."', '".$_POST['zarobki']."', '".$_POST['data_']."')";
+   if ($conn->query($sql) === TRUE) {
+    echo("<br>"); 
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
+?>
 ?>
 
