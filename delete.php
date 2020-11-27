76c49476e833a91b4f5a5d215f9660e1d21c1bd3
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2020-11-27 08:54:52 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -19,8 +19,16 @@
   <a class="nav_link" href="function.php">funkcja</a>
 </div>
 <?php
+   echo ("<h1>id: ".$_POST['id']."</h1>");
+require_once("connect.php");
 
+ $sql = "DELETE FROM pracownicy WHERE id_pracownicy='".$_POST['id']."'";
 
+if ($conn->query($sql) === TRUE) {
+        echo("<h1 class='precord'> Usunięto pracownika </h1>");
+      } else {
+        echo("<h1 class='precord'>'Error: ' . $sql . '<br>' . $conn->error</h1>");
+      }
     
       
 ?>
