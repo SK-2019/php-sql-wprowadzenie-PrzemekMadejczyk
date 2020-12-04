940c9d9d0c31f443bef75a100993b678f831c7bf
Your Name <you@example.com>
2020-12-04 14:05:08 +0100

    sad

--- ksiazki/wypozycenie.php
+++ ksiazki/wypozycenie.php
@@ -1,9 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  </form
+</head>
+  <link rel="stylesheet" href="/style.css">
+<body>
+<strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a></strong>
+<h1>Przemek Madejczyk 2ti 20</h1>
+<div class="nav">
+    <a class="nav_link" href="/pracownicy.php">Pracownicy - wstęp</a>
+    <a class="nav_link" href="/funkcjeagregujace.php">Funkcje Agregujące</a>
+    <a class="nav_link" href="/pracownicy i organizacja.php">Pracownicy i Organizacja</a>
+  <a class="nav_link" href="/index.php">strona głowna</a>
+  <a class="nav_link" href="/Data_czas.php">Data i czas</a>
+  <a class="nav_link" href="/formularz.html">Formularze</a>
+  <a class="nav_link" href="/daneDoBazy.php">dane do bazy danych</a>
+  <a class="nav_link" href="/function.php">funkcja</a>
+  
+</div>
 <?php
-
-
-
 echo("<h1>Wypożyczone książki:</h1>");
-$sql = 'SELECT * from bibl_wypoz';
+$sql = 'SELECT * from bibl_wypozycenie';
 echo("<li>".$sql);
 
 $result = $conn->query($sql);
