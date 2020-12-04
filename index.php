049a2175b2b876485bd41b96236b7c279d3bcb6a
Your Name <you@example.com>
2020-12-04 09:35:33 +0100

    a

--- index.php
+++ index.php
@@ -10,15 +10,15 @@
 <strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a></strong>
 <h1>Przemek Madejczyk 2ti 20</h1>
 <div class="nav">
+    <a class="nav_link" href="index.php">strona głowna</a>
     <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
     <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
     <a class="nav_link" href="pracownicy i organizacja.php">Pracownicy i Organizacja</a>
-  <a class="nav_link" href="index.php">strona głowna</a>
-  <a class="nav_link" href="Data_czas.php">Data i czas</a>
-  <a class="nav_link" href="formularz.html">Formularze</a>
-  <a class="nav_link" href="daneDoBazy.php">dane do bazy danych</a>
-  <a class="nav_link" href="function.php">funkcja</a>
-<a  class="nav1" href="ksiazki/ksiazki.php"><b>Książki</b></a>
+    <a class="nav_link" href="Data_czas.php">Data i czas</a>
+    <a class="nav_link" href="formularz.html">Formularze</a>
+    <a class="nav_link" href="daneDoBazy.php">dane do bazy danych</a>
+    <a class="nav_link" href="function.php">funkcja</a>
+    <a class="nav1" href="ksiazki/ksiazki.php"><b>Książki</b></a>
   
 </div>
 <?php
@@ -85,5 +85,17 @@ require_once('conn.php');
         echo("</table>");
         echo("<hr />");
 
-$conn->close();
+        $result=$conn->query('SELECT id_krzyz as id,tytul,imie,nazwisko FROM `bibl_krzyz`,bibl_tytul,biblautor where biblautor.id_autor=bibl_krzyz.id_autor and bibl_tytul.id_tytul=bibl_krzyz.id_tytul');
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>tytul</th>");
+        echo("<th>imie</th>");
+        echo("<th>nazwisko</th>");
+        echo("<th>Usuń</th>");    
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                echo("<td>".$row['id']."</td><td>".$row['tytul']."</td><td>".$row['imie']."</td><td>".$row['nazwisko']."</td>");            
+                echo("</tr>");
+            }
+        echo("</table>");
 ?>
