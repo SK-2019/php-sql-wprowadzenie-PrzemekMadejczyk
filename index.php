50762f0834fad977cb38315bb1556e511f4e8bcf
Your Name <you@example.com>
2021-01-26 14:41:15 +0100

    a

--- index.php
+++ index.php
@@ -11,12 +11,12 @@
 <h1>Przemek Madejczyk 2ti 20</h1>
 <div class="nav">
     <a class="nav_link" href="index.php">strona głowna</a>
-    <a class="nav_link" href="pracownicy.php">Pracownicy - wstęp</a>
-    <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
-    <a class="nav_link" href="pracownicy i organizacja.php">Pracownicy i Organizacja</a>
-    <a class="nav_link" href="Data_czas.php">Data i czas</a>
-    <a class="nav_link" href="formularz.html">Formularze</a>
-    <a class="nav_link" href="daneDoBazy.php">dane do bazy danych</a>
+    <a class="nav_link" href="pracownicy/pracownicy.php">Pracownicy - wstęp</a>
+    <a class="nav_link" href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a>
+    <a class="nav_link" href="pracownicy/pracownicy i organizacja.php">Pracownicy i Organizacja</a>
+    <a class="nav_link" href="pracownicy/Data_czas.php">Data i czas</a>
+    <a class="nav_link" href="pracownicyformularz.html">Formularze</a>
+    <a class="nav_link" href="pracownicydaneDoBazy.php">dane do bazy danych</a>
     <a class="nav_link" href="function.php">funkcja</a>
     <a class="nav1" href="ksiazki/ksiazki.php"><b>Książki</b></a>
   
