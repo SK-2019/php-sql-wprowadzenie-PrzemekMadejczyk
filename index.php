14f482a31565039a5dfcd6b11d7ba03d608e86ef
Your Name <you@example.com>
2021-01-26 14:17:46 +0100

    a

--- index.php
+++ index.php
@@ -32,12 +32,12 @@ require_once('assets/conn.php');
         echo("<th>id</th>");
         echo("<th>imię</th>");
         echo("<th>dział</th>");
-        echo("<th>zarobki2</th>");
+        echo("<th>zarobki</th>");
         echo("<th>nazwa działu</th>");
         echo("<th>data urodzenia</th>");
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki2']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
                 echo("</tr>");
             }
         echo("</table>");
@@ -53,13 +53,13 @@ require_once('assets/conn.php');
         echo("<th>id</th>");
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
-        echo("<th>zarobki2</th>");
+        echo("<th>zarobki</th>");
         echo("<th>nazwa_dzial</th>");
         echo("<th>data_urodzenia</th>");
    
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki2']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
                 echo("</tr>");
             }
         echo("</table>");
@@ -73,13 +73,13 @@ require_once('assets/conn.php');
         echo("<th>id</th>");
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
-        echo("<th>zarobki2</th>");
+        echo("<th>zarobki</th>");
         echo("<th>nazwa_dzial</th>");
         echo("<th>data_urodzenia</th>");
  
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki2']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
                 echo("</tr>");
             }
         echo("</table>");
