12585e1a764d5785950892bef57fa19a1f37a71f
Your Name <you@example.com>
2021-01-26 13:32:32 +0100

    asd

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
   </form
 </head>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="/style.css">
 <body>
 <strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a></strong>
 <h1>Przemek Madejczyk 2ti 20</h1>
@@ -22,7 +22,7 @@
   
 </div>
 <?php
-require_once('conn.php');
+require_once('/conn.php');
         $sql=('SELECT * FROM pracownicy,organizacja where dzial=id_org');
     $result=$conn->query($sql);
         echo("<hr />");
@@ -73,13 +73,13 @@ require_once('conn.php');
         echo("<th>id</th>");
         echo("<th>imie</th>");
         echo("<th>dzial</th>");
-        echo("<th>zarobki2</th>");
+        echo("<th>zarobki</th>");
         echo("<th>nazwa_dzial</th>");
         echo("<th>data_urodzenia</th>");
  
             while($row=$result->fetch_assoc()){
                 echo("<tr>");
-                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki2']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['data_urodzenia']."</td>");
                 echo("</tr>");
             }
         echo("</table>");
