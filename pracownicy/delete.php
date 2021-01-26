610cfb43696d18a1d4437f38fb412988ac914683
Your Name <you@example.com>
2021-01-26 14:52:44 +0100

    asd

--- pracownicy/delete.php
+++ pracownicy/delete.php
@@ -9,14 +9,15 @@
 <strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a></strong>
 <h1>Przemek Madejczyk 2ti 20</h1>
 <div class="nav">
-      <a class="nav_link" href=/pracownicy.php">Pracownicy - wstęp</a>
-    <a class="nav_link" href="/funkcjeagregujace.php">Funkcje Agregujące</a>
-    <a class="nav_link" href="/pracownicy i organizacja.php">Pracownicy i Organizacja</a>
-  <a class="nav_link" href="/index.php">strona głowna</a>
-  <a class="nav_link" href="/Data_czas.php">Data i czas</a>
-  <a class="nav_link" href="/formularz.html">Formularze</a>
-  <a class="nav_link" href="/daneDoBazy.php">dane do bazy danych</a>
-  <a class="nav_link" href="/function.php">funkcja</a>
+<a class="nav_link" href="pracownicy/pracownicy.php">Pracownicy - wstęp</a>
+    <a class="nav_link" href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a>
+    <a class="nav_link" href="pracownicy/pracownicy i organizacja.php">Pracownicy i Organizacja</a>
+  <a class="nav_link" href="../index.php">strona głowna</a>
+  <a class="nav_link" href="pracownicy/Data_czas.php">Data i czas</a>
+  <a class="nav_link" href="cwiczenia/formularz.html">Formularze</a>
+  <a class="nav_link" href="pracownicy/daneDoBazy.php">dane do bazy danych</a>
+  <a class="nav_link" href="cwiczenia/function.php">funkcja</a>
+  <a class="nav1" href="../ksiazki/ksiazki.php"><b>Książki</b></a>
 </div>
 <?php
    echo ("<h1>id: ".$_POST['id']."</h1>");
