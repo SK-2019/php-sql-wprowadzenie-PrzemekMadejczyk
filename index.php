d3c493cfc977e24cf036aa8d0ec3f0e16890b108
Your Name <you@example.com>
2021-01-28 11:54:59 +0100

    a

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a></strong>
 <h1>Przemek Madejczyk 2ti 20</h1>
 <div class="nav">
+<a class ="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a>
     <a class="nav_link" href="index.php">strona głowna</a>
     <a class="nav_link" href="pracownicy/pracownicy.php">Pracownicy - wstęp</a>
     <a class="nav_link" href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a>
@@ -18,7 +19,7 @@
     <a class="nav_link" href="pracownicy/formularz.html">Formularze</a>
     <a class="nav_link" href="pracownicy/daneDoBazy.php">dane do bazy danych</a>
     <a class="nav_link" href="function.php">funkcja</a>
-    <a class="nav1" href="ksiazki/ksiazki.php"><b>Książki</b></a>
+    <a class="nav_link" href="ksiazki/ksiazki.php"><b>Książki</b></a>
   
 </div>
 
