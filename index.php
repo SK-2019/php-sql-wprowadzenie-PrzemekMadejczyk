a3a88f0b0abfc062e16973d49193ed85820c81af
Your Name <you@example.com>
2021-01-28 11:30:04 +0100

    a

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
 <strong><a href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a></strong>
 <h1>Przemek Madejczyk 2ti 20</h1>
-<div class="sidebar" id="mySidenav">
+<div class="nav">
     <a class="nav_link" href="index.php">strona głowna</a>
     <a class="nav_link" href="pracownicy/pracownicy.php">Pracownicy - wstęp</a>
     <a class="nav_link" href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a>
