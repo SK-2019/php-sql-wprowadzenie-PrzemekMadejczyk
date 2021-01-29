a2d94f0b72c31ed02ad805795640148661f91f2d
Your Name <you@example.com>
2021-01-29 13:04:37 +0100

    a

--- pracownicy/pracownicy.php
+++ pracownicy/pracownicy.php
@@ -18,7 +18,7 @@
   <a class="nav_link" href="cwiczenia/formularz.html">Formularze</a>
   <a class="nav_link" href="daneDoBazy.php">dane do bazy danych</a>
   <a class="nav_link" href="cwiczenia/function.php">funkcja</a>
-  <a class="nav1" href="../ksiazki/ksiazki.php"><b>Książki</b>
+  <a class="nav1" href="../ksiazki/ksiazki.php"><a>Książki</a>
 </div>
     <h1>Pracownicy</h1>
 <?php
