946c0fbcd7c7f6f2655c8bb338ad84dd84cefef4
Your Name <you@example.com>
2021-01-29 13:25:28 +0100

    a

--- pracownicy/daneDoBazy.php
+++ pracownicy/daneDoBazy.php
@@ -11,10 +11,11 @@
 <h1>Przemek Madejczyk 2ti 20</h1>
 <div class="nav">
 <a class ="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a>
+<a class="nav_link" href="../index.php">strona głowna</a>
      <a class="nav_link" href="pracownicy/pracownicy.php">Pracownicy - wstęp</a>
     <a class="nav_link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
     <a class="nav_link" href="pracownicy i organizacja.php">Pracownicy i Organizacja</a>
-  <a class="nav_link" href="../index.php">strona głowna</a>
+ 
   <a class="nav_link" href="Data_czas.php">Data i czas</a>
   <a class="nav_link" href="cwiczenia/formularz.html">Formularze</a>
   <a class="nav_link" href="daneDoBazy.php">dane do bazy danych</a>
