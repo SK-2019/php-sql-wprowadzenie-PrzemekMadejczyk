945f93f4f0b5c4be307705b06897ec2b0606e7ee
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2021-04-13 20:26:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <a class="nav_link" href="pracownicy/daneDoBazy.php">dane do bazy danych</a>
     <a class="nav_link" href="cwiczenia/function.php">funkcja</a>
     <a class="nav_link" href="ksiazki/ksiazki.php"><b>Książki</b></a>
-  
+<a class="nav_link" href="><b>flexbox</b></a>  
 </div>
 
 
