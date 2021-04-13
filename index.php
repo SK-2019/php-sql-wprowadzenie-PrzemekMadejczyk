4b315b796327cdd2c71c478415ece53fcf0df969
Przemek-Madejczyk <73532007+PrzemekMadejczyk@users.noreply.github.com>
2021-04-13 20:55:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 <from>
-<title>Przemek Madejczyk 122Ti Gr2</title>
+le>
 <link rel="stylesheet" href="assets/style.css">
 <link rel="icon" href="https://www.streamscheme.com/wp-content/uploads/2020/04/pepega.png" type="image/icon type">
   </from>
@@ -11,7 +11,7 @@
   
 <body>
 
-<h1>Przemek Madejczyk 112ti 20</h1>
+<h1>Przemek Madejczyk 2ti 20</h1>
 <div class="nav">
 <a class ="nav_link" href="https://www.pinclipart.com/picdir/middle/104-1042073_variety-of-services-money-bag-icon-png-clipart.png">Github</a>
     <a class="nav_link" href="index.php">strona głowna</a>
