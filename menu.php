0d36ac4925967e5512ec769f9cc1c4d2be688201
Your Name <you@example.com>
2021-04-20 13:38:13 +0200

    a

--- menu.php
+++ menu.php
@@ -1,14 +1,14 @@
 <ul class="nav">
 <a class ="nav_link" href="https://www.pinclipart.com/picdir/middle/104-1042073_variety-of-services-money-bag-icon-png-clipart.png">Github</a>
-   <li class="nav_link" href="index.php">strona głowna</a></li>
-   <li class="nav_link" href="pracownicy/pracownicy.php">Pracownicy - wstęp</a></li>
-   <li class="nav_link" href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a></li>
-   <li class="nav_link" href="pracownicy/pracownicy i organizacja.php">Pracownicy i Organizacja</a></li>
-   <li class="nav_link" href="pracownicy/Data_czas.php">Data i czas</a></li>
-   <li class="nav_link" href="cwiczenia/formularz.html">Formularze</a></li>
-   <li class="nav_link" href="pracownicy/daneDoBazy.php">dane do bazy danych</a></li>
-   <li class="nav_link" href="cwiczenia/function.php">funkcja</a></li>
-   <li class="nav_link" href="ksiazki/ksiazki.php"><b>Książki</a></li>
-   <li class="nav_link" href="flexbox.html"><b>flexbox</a></li>
-   <li class="nav_link" href="wiele_do_wielu.php"><b>wiele_do_wielu</a></li>
+     <li class="nav_item"><a href="/index.php">strona główna</a></li>
+     <li class="nav_item"><a href="pracownicy/pracownicy.php">Pracownicy - wstęp</a></li>
+     <li class="nav_item"><a href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a></li>
+     <li class="nav_item"><a href="pracownicy/pracownicy i organizacja.php">Pracownicy i Organizacja</a></li>
+     <li class="nav_item"><a href="pracownicy/Data_czas.php">Data i czas</a></li>
+     <li class="nav_item"><a href="cwiczenia/formularz.html">Formularze</a></li>
+     <li class="nav_item"><a href="pracownicy/daneDoBazy.php">dane do bazy danych</a></li>
+     <li class="nav_item"><a href="cwiczenia/function.php">funkcja</a></li>
+     <li class="nav_item"><a href="ksiazki/ksiazki.php"><b>Książki</a></li>
+     <li class="nav_item"><a href="/flexbox/flexbox.html">Flexbox</a></li>
+     <li class="nav_item"><a href="wiele_do_wielu.php"><b>wiele_do_wielu</a></li>
 </ul>
