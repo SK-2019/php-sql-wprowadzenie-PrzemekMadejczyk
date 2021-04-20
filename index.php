4342d80b007d0edccde2675af2041f28834e4a3b
Your Name <you@example.com>
2021-04-20 12:54:11 +0200

    a

--- index.php
+++ index.php
@@ -8,24 +8,19 @@
 <link rel="icon" href="https://www.streamscheme.com/wp-content/uploads/2020/04/pepega.png" type="image/icon type">
   </from>
 </head>
-  
 <body>
-
 <h1>Przemek Madejczyk 2ti 20</h1>
 <div class="nav">
-<a class ="nav_link" href="https://www.pinclipart.com/picdir/middle/104-1042073_variety-of-services-money-bag-icon-png-clipart.png">Github</a>
-    <a class="nav_link" href="index.php">strona głowna</a>
-    <a class="nav_link" href="pracownicy/pracownicy.php">Pracownicy - wstęp</a>
-    <a class="nav_link" href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a>
-    <a class="nav_link" href="pracownicy/pracownicy i organizacja.php">Pracownicy i Organizacja</a>
-    <a class="nav_link" href="pracownicy/Data_czas.php">Data i czas</a>
-    <a class="nav_link" href="cwiczenia/formularz.html">Formularze</a>
-    <a class="nav_link" href="pracownicy/daneDoBazy.php">dane do bazy danych</a>
-    <a class="nav_link" href="cwiczenia/function.php">funkcja</a>
-    <a class="nav_link" href="ksiazki/ksiazki.php"><b>Książki</b></a>
-    <a class="nav_link" href="flexbox.html"><b>flexbox</b></a>
-    <a class="nav_link" href="wiele_do_wielu.php"><b>wiele_do_wielu</b></a>
+<body>
+    <div class="container">
+      <div class="header">
+        <?php include("header.php"); ?>
+      </div>
+      <div class="menu">
+      <?php include("menu.php"); ?>
+      </div>
+      <div class="main"></div>
+    </div>
+  </body>
 </div>
-<?php
- require_once('assets/conn.php');
-?>
+</html>
---
+++ menu.php
@@ -0,0 +1,14 @@
+<ul class="nav">
+<a class ="nav_link" href="https://www.pinclipart.com/picdir/middle/104-1042073_variety-of-services-money-bag-icon-png-clipart.png">Github</a>
+    <a class="nav_link" href="index.php">strona głowna</a>
+    <a class="nav_link" href="pracownicy/pracownicy.php">Pracownicy - wstęp</a>
+    <a class="nav_link" href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a>
+    <a class="nav_link" href="pracownicy/pracownicy i organizacja.php">Pracownicy i Organizacja</a>
+    <a class="nav_link" href="pracownicy/Data_czas.php">Data i czas</a>
+    <a class="nav_link" href="cwiczenia/formularz.html">Formularze</a>
+    <a class="nav_link" href="pracownicy/daneDoBazy.php">dane do bazy danych</a>
+    <a class="nav_link" href="cwiczenia/function.php">funkcja</a>
+    <a class="nav_link" href="ksiazki/ksiazki.php"><b>Książki</b></a>
+    <a class="nav_link" href="flexbox.html"><b>flexbox</b></a>
+    <a class="nav_link" href="wiele_do_wielu.php"><b>wiele_do_wielu</b></a>
+</ul>
