16a950e4d1fc43d65ad8400c25fa04d998be0e48
Your Name <you@example.com>
2021-04-20 14:13:14 +0200

    a

--- menu.php
+++ menu.php
@@ -1,14 +1,14 @@
 <ul class="nav">
 <a class ="nav_link" href="https://github.com/SK-2019/php-sql-wprowadzenie-PrzemekMadejczyk">Github</a>
-     <li class="nav_link"><a href="/index.php">strona główna</a></li>
-     <li class="nav_link"><a href="/pracownicy/pracownicy.php">Pracownicy - wstęp</a></li>
-     <li class="nav_link"><a href="/pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a></li>
-     <li class="nav_link"><a href="/pracownicy/pracownicy i organizacja.php">Pracownicy i Organizacja</a></li>
-     <li class="nav_link"><a href="/pracownicy/Data_czas.php">Data i czas</a></li>
-     <li class="nav_link"><a href="/cwiczenia/formularz.html">Formularze</a></li>
-     <li class="nav_link"><a href="/pracownicy/daneDoBazy.php">dane do bazy danych</a></li>
-     <li class="nav_link"><a href="/cwiczenia/function.php">funkcja</a></li>
-     <li class="nav_link"><a href="/ksiazki/ksiazki.php">Książki</a></li>
-     <li class="nav_link"><a href="/flexbox/flexbox.html">Flexbox</a></li>
-     <li class="nav_link"><a href="/wiele_do_wielu.php">wiele_do_wielu</a></li>
+     <a class ="nav_link"  href="/index.php">strona główna</a>
+     <a class ="nav_link"  href="/pracownicy/pracownicy.php">Pracownicy - wstęp</a>
+     <a class ="nav_link"  href="/pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a>
+     <a class ="nav_link"  href="/pracownicy/pracownicy i organizacja.php">Pracownicy i Organizacja</a>
+     <a class ="nav_link"  href="/pracownicy/Data_czas.php">Data i czas</a>
+     <a class ="nav_link"  href="/cwiczenia/formularz.html">Formularze</a>
+     <a class ="nav_link"  href="/pracownicy/daneDoBazy.php">dane do bazy danych</a>
+     <a class ="nav_link" href="/cwiczenia/function.php">funkcja</a>
+     <a class ="nav_link"  href="/ksiazki/ksiazki.php">Książki</a>
+     <a class ="nav_link"  href="/flexbox/flexbox.html">Flexbox</a>
+     <a class ="nav_link"  href="/wiele_do_wielu.php">wiele_do_wielu</a>
 </ul>
