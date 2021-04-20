9c4e76a7c7953747d40b6d1c45bf35a6ce0c91b0
Your Name <you@example.com>
2021-04-20 13:34:49 +0200

    a

--- menu.php
+++ menu.php
@@ -1,14 +1,14 @@
 <ul class="nav">
 <a class ="nav_link" href="https://www.pinclipart.com/picdir/middle/104-1042073_variety-of-services-money-bag-icon-png-clipart.png">Github</a>
-    <a class="nav_link" href="index.php">strona głowna</a>
-    <a class="nav_link" href="pracownicy/pracownicy.php">Pracownicy - wstęp</a>
-    <a class="nav_link" href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a>
-    <a class="nav_link" href="pracownicy/pracownicy i organizacja.php">Pracownicy i Organizacja</a>
-    <a class="nav_link" href="pracownicy/Data_czas.php">Data i czas</a>
-    <a class="nav_link" href="cwiczenia/formularz.html">Formularze</a>
-    <a class="nav_link" href="pracownicy/daneDoBazy.php">dane do bazy danych</a>
-    <a class="nav_link" href="cwiczenia/function.php">funkcja</a>
-    <a class="nav_link" href="ksiazki/ksiazki.php"><b>Książki</a>
-    <a class="nav_link" href="flexbox.html"><b>flexbox</a>
-    <a class="nav_link" href="wiele_do_wielu.php"><b>wiele_do_wielu</a>
+   <li class="nav_link" href="index.php">strona głowna</a></li>
+   <li class="nav_link" href="pracownicy/pracownicy.php">Pracownicy - wstęp</a></li>
+   <li class="nav_link" href="pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a></li>
+   <li class="nav_link" href="pracownicy/pracownicy i organizacja.php">Pracownicy i Organizacja</a></li>
+   <li class="nav_link" href="pracownicy/Data_czas.php">Data i czas</a></li>
+   <li class="nav_link" href="cwiczenia/formularz.html">Formularze</a></li>
+   <li class="nav_link" href="pracownicy/daneDoBazy.php">dane do bazy danych</a></li>
+   <li class="nav_link" href="cwiczenia/function.php">funkcja</a></li>
+   <li class="nav_link" href="ksiazki/ksiazki.php"><b>Książki</a></li>
+   <li class="nav_link" href="flexbox.html"><b>flexbox</a></li>
+   <li class="nav_link" href="wiele_do_wielu.php"><b>wiele_do_wielu</a></li>
 </ul>
