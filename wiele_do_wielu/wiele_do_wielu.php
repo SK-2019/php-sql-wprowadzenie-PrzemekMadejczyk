a445c8a4d030d87f53941355324ea92cc20b1fa5
Your Name <you@example.com>
2021-04-27 11:54:01 +0200

    A

--- wiele_do_wielu/wiele_do_wielu.php
+++ wiele_do_wielu/wiele_do_wielu.php
@@ -19,7 +19,7 @@
     <div class="main">
 <?php
 echo('<h1>autor i ksiazki</h1>')
- require_once("../assets/conn.php");
+       require_once('../assets/conn.php');  
  $sql="Select * from autor, tytul, autor_tytul where autor.id=autor_id and tytul.id=tytul_id";
  $result=$conn->query($sql);
  echo("<li>SQL:  $sql ");
@@ -54,9 +54,11 @@ echo('<h1>autor i ksiazki</h1>')
         echo("<table border=1>");
         echo("<th>id</th>");
         echo("<th>Tytul</th>");
-                while($row=$result->fetch_assoc()){
+                while($row=$result->fetch_assoc())
+                {
                     echo("<tr>");
                         echo("<td>".$row["id"]."</td><td>".$row["tytul"]."</td>");
-                    echo("</tr>");}
+                    echo("</tr>");
+                }
         echo("</table>"); 
 ?>
