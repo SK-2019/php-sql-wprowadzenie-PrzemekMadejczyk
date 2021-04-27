141af56714e782c5f73ea3a2da4a6d3e126ccdcf
Your Name <you@example.com>
2021-04-27 11:39:34 +0200

    a

--- wiele_do_wielu/wiele_do_wielu.php
+++ wiele_do_wielu/wiele_do_wielu.php
@@ -21,7 +21,7 @@
     <div class="main">
 <?php
 echo('<h1>autor i ksiazki</h1>')
- require_once("../assets/conn.php");
+ require_once("../assets/conn.php");`
  $sql="Select * from autor, tytul, autor_tytul where autor.id=autor_id and tytul.id=tytul_id";
  $result=$conn->query($sql);
  echo("<li>SQL:  $sql ");
